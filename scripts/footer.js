45e30aece1f3280603cc29f99867e87053b0e80e
Zichqec <36972872+Zichqec@users.noreply.github.com>
2023-10-22 20:22:39 -0400

    Add github link to footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,6 +2,7 @@ function createFooter()
 {
 	return `<p class="footertext">More from Zichqec<br>
 	<a rel="me" href="https://ukadon.shillest.net/@Zichqec">Ukadon</a> |
+	<a href="https://github.com/zichqec">GitHub</a> |
 	<a href="https://zichqec.tumblr.com/">Tumblr</a> | 
 	<a href="https://www.deviantart.com/zichqec">Deviantart</a> | 
 	<a href="https://twitter.com/Zichqec">Twitter</a>`;
