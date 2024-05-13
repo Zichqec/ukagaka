17b94efd72a606602681a0daa56da0bf09b9b856
Zichqec <36972872+Zichqec@users.noreply.github.com>
2024-05-13 14:11:34 -0400

    Add Galaxy Blob

--- scripts/misc_details.js
+++ scripts/misc_details.js
@@ -11,10 +11,20 @@ let item_details = [
 		blurb: "A simple dark theme mod for the original version of Tama! This may work with the open source version, but I make no guarantees.<br><br>This simply changes the color values in Tama.txt to be a pleasing dark theme. WHILE TAMA IS CLOSED, replace your original tama.txt with this file. Then start Tama again, and it should appear in dark mode."
 	},
 	{
+		name: "Galaxy Blob",
+		release: "September 25th, 2022",
+		latest: null,
+		version: null,
+		forevent: null,
+		goeswith: [{name: "Anti Stress Blob", download: "https://zdzisiu.github.io/Cemetery/work_index.html?type=ghost#antistress_blob"}],
+		collaborators: null,
+		download: "https://www.mediafire.com/file/9froflig23yvtd7/galaxy_blob.nar/file",
+		blurb: "A supplement file for <a href='https://zdzisiu.github.io/Cemetery/index.html'>Internet Teacup's</a> Anti Stress Blob, which changes the color of the blob to a galaxy pattern. The galaxy pattern was kindly provided to me by Galla, and matches the galaxy pattern used in their <a href'../ghost/hydrate.html'>Hydrate</a> shell, <a href='https://gallathegalla.github.io/gtg-ghosts/shells/hydrate'>Slimedration</a>.<br><br>This is <em>not</em> a separate shell, it <em>will</em> overwrite whatever colors you are currently using for your blob."
+	},
+	{
 		name: "Galaxy Parrot",
 		release: "May 12th, 2024",
 		latest: null,
-		version: "1.0.0",
 		forevent: null,
 		goeswith: [{name: "Party Parrot", download: "https://github.com/nikolat/ukagaka-party-parrot"}],
 		collaborators: null,
