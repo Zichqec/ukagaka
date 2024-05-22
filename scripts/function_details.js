82a3fcb78effd4f8a2428bf8c0d4eeba83da54cf
Zichqec <36972872+Zichqec@users.noreply.github.com>
2024-05-22 17:50:30 -0400

    Music Player v2.0.2

--- scripts/function_details.js
+++ scripts/function_details.js
@@ -223,7 +223,7 @@ let item_details = [
 		name: "Music player",
 		release: "April 29th, 2022",
 		latest: "May 22nd, 2024",
-		version: "2.0.1",
+		version: "2.0.2",
 		forevent: null,
 		language: "YAYA",
 		download: "https://github.com/Zichqec/music_player/releases/latest",
