cd432591bfc6737f2ce52a8a531ecbbfcf1a2aed
Zichqec <36972872+Zichqec@users.noreply.github.com>
2025-09-01 06:24:57 -0700

    Missing/broken tags

--- scripts/footer.js
+++ scripts/footer.js
@@ -5,7 +5,7 @@ function createFooter()
 	<a href="https://github.com/zichqec">GitHub</a> |
 	<a href="https://zichqec.tumblr.com/">Tumblr</a> | 
 	<a href="https://www.deviantart.com/zichqec">Deviantart</a> | 
-	<a href="https://twitter.com/Zichqec">Twitter</a>`;
+	<a href="https://twitter.com/Zichqec">Twitter</a></p>`;
 }
 
 document.getElementById('footer').innerHTML = createFooter();
