67794d4cb2c820186ac5c0e33f5d60300069ceab
Zichqec <36972872+Zichqec@users.noreply.github.com>
2025-09-15 14:10:24 -0400

    Hydrate v2.0.2

--- scripts/ghost_details.js
+++ scripts/ghost_details.js
@@ -273,8 +273,8 @@ var item_details = [
 	{
 		name: "Hydrate",
 		release: "2020-08-20",
-		latest: "2025-09-14",
-		version: "v2.0.1",
+		latest: "2025-09-15",
+		version: "v2.0.2",
 		forevent: null,
 		collaborators:
 		[
