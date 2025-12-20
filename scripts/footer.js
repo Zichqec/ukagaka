57d4c7f13e977385f4451074632f68d72ae82831
Zichqec <36972872+Zichqec@users.noreply.github.com>
2025-12-20 11:17:11 -0500

    Add UDT forum to footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,7 @@
 function createFooter()
 {
 	return `<p class="footertext">More from Zichqec<br>
+	<a href="https://forum.ukagakadreamteam.com/u/Zichqec">UDT forum</a> |
 	<a rel="me" href="https://ukadon.shillest.net/@Zichqec">Ukadon</a> |
 	<a href="https://github.com/zichqec">GitHub</a> |
 	<a href="https://zichqec.tumblr.com/">Tumblr</a> | 
