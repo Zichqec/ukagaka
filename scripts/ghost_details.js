346705dfb9d816bf557d402821e6e59a0c52dad8
Zichqec <36972872+Zichqec@users.noreply.github.com>
2026-07-31 13:57:14 -0400

    ATM Machine v2.0.3

--- scripts/ghost_details.js
+++ scripts/ghost_details.js
@@ -32,7 +32,7 @@ var item_details = [
 	{
 		name: "ATM Machine",
 		release: "2023-04-09",
-		latest: "2025-07-28",
+		latest: "2026-07-31",
 		version: "v2.0.2",
 		forevent: null,
 		collaborators: null,
